643b3d8b1f34a9f390d799f3e9eec0f4e15281a0
uXylonZ <113145321+uXylonZ@users.noreply.github.com>
2023-06-25 23:58:40 +0100

    Add files via upload

---
+++ event_base.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+const { QuickDB } = require('quick.db');
+const db = new QuickDB();
+const config = require("../config.json")
+require("colors")
+
+module.exports = {
+    name: 'aaaaaaaa',
+    async execute(client, interaction, commands, member) {
+        
+        // asdasd
+
+    }
+}
